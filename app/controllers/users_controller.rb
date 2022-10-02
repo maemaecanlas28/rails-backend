3b9339498f8363fe51724f65f26111c5767c0406
Flordeliza Canlas <maemaeperlas@gmail.com>
2022-10-02 12:54:49 -0700

    made some changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    skip_before_action :authorize, only: [:index, :create]
+    skip_before_action :authorize, only: [:index, :show, :create]
 
     def index
         render json: User.all
